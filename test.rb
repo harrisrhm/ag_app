2c2dfba5e65067dec7a3403a81cf47f85c38df0a
Harris Rahim <harris.rhm@gmail.com>
2021-05-31 06:34:35 +1000

    Imported items.json into indices

---
+++ test.rb
@@ -0,0 +1,9 @@
+require 'algolia'
+require 'json'
+
+client = Algolia::Search::Client.create('LG58UB0A5R', '29cb07ffc76d1661e129ecbdf15ad8ac')
+index = client.init_index('ag')
+
+index = client.init_index('items')
+batch = JSON.parse(File.read('items.json'))
+index.save_objects(batch)
